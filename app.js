3d777cdc032405ebe755b0815abb02d285f39364
kimminsic <alstlr1135@gmail.com>
2022-05-25 17:52:27 +0900

    할일리스트 API 구현

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 import express from "express";
 import cors from "cors";
+import mysql from "mysql2/promise";
+
+const pool = mysql.createPool({
+  host : "localhost",
+  user: "sbsst",
+  password: "sbs123414",
+  database: "todoapp_2022_05_25",
+  waitForConnections:true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  dateStrings: true,
+});
 
 const app = express();
 
@@ -13,15 +25,22 @@ app.use(express.json());
 
 const appPort = 3000;
 
-app.get(`/todos`,(req,res)=>{
-return res.json([
-  {
-id: 1,
-  },
-  {
-id: 2,
-  }
-]);
+app.get(`/:user_id/todos`, async (req,res)=>{
+  const {user_id}=req.params;
+
+  const [todoRows] = await pool.query(`
+  SELECT *
+  FROM todo 
+  WHERE user_id = ?
+  ORDER BY id DESC`,
+  [user_id]
+  );
+
+  res.json({
+    resultCode:"S-1",
+    msg:"성공",
+    data: todoRows
+  });
 });
 
 app.listen(appPort, () =>{
